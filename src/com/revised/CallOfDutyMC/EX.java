1f364efef02f0275dc90dea373e207acd347d598
emericask8ur <sk8tj@yahoo.com>
2012-02-06 21:41:11 -0500

    Methods for Buying Weapon Classes

---
+++ src/com/revised/CallOfDutyMC/EX.java
@@ -0,0 +1,16 @@
+package com.revised.CallOfDutyMC;
+
+import org.bukkit.entity.Player;
+
+public class EX {
+	
+	public static void buyClassA(Player player){
+		player.setExp(-10);
+		
+	}
+	
+	public static void buyClassB(Player player){
+		player.setExp(-20);
+	
+	}
+}
